9b4966b564afbc07ac5d1cd6aae2c679e54f97c2
Sodiq Fagbola <fagbola@systemspecs.com.ng>
2017-07-23 10:23:40 +0100

    created project

---
+++ src/main/java/com/openbanking/api/ng/ApiNgApplication.java
@@ -0,0 +1,12 @@
+package com.openbanking.api.ng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiNgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiNgApplication.class, args);
+	}
+}
